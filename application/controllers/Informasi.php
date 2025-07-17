5ed81859be4f2ce7406c9c69a11d20834811232c
Yulie27 <lieeyulie27@gmail.com>
2025-07-17 12:25:00 +0700

    add informasi penolakan

--- application/controllers/Informasi.php
+++ application/controllers/Informasi.php
@@ -14,30 +14,43 @@ class Informasi extends CI_Controller
         $data['total_subsidi'] = $this->db->get('subsidi_pupuk')->num_rows();
         $data['title'] = 'Informasi';
         $this->load->view('informasi/infor', $data);
-        // $this->load->view('informasi/index', $data);
     }
 
     public function find($nik)
     {
         $is_exist_nik = $this->db->get_where('petani', array('nik' => $nik))->num_rows() > 0;
 
-        $response;
-        if ($is_exist_nik) {
-            $query = "SELECT subsidi_pupuk.id_subsidi
-                FROM subsidi_pupuk
-                LEFT JOIN petani ON subsidi_pupuk.id_petani = petani.id_petani
-                WHERE petani.nik = '$nik'";
-            $result = $this->db->query($query)->row();
-
-            if ($result) {
-                $response = ['message' => 'Selamat!!. Anda mendapat subsidi pupuk'];
-            } else {
-                $response = ['message' => "Anda Belum mendapatkan subsidi pupuk"];
-            }
-        } else {
+        if (!$is_exist_nik) {
             $response = ['message' => 'NIK Tidak Ditemukan'];
+            echo json_encode($response);
+            return;
         }
 
+        $query = "SELECT subsidi_pupuk.*
+            FROM subsidi_pupuk
+            LEFT JOIN petani ON subsidi_pupuk.id_petani = petani.id_petani
+            WHERE petani.nik = '$nik'";
+        $result = $this->db->query($query)->row();
+
+        if (is_null($result)) {
+            $response = ['message' => "Anda Belum mendapatkan subsidi pupuk"];
+            echo json_encode($response);
+            return;
+        }
+
+        if ($result->validasi_desa == 'ditolak') {
+            $response = ['message' => 'Pengajuan ditolak oleh desa : ' . $result->penolakan_desa];
+            echo json_encode($response);
+            return;
+        }
+
+        if ($result->validasi_bpp == 'ditolak') {
+            $response = ['message' => 'Pengajuan ditolak oleh bpp : ' . $result->penolakan_bpp];
+            echo json_encode($response);
+            return;
+        }
+
+        $response = ['message' => 'Selamat!!. Anda mendapat subsidi pupuk'];
         echo json_encode($response);
     }
 }
